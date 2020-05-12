05413916f9d5d57bdeb572d81ba3b274cfb97e75
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 20:47:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@ require 'pry'
 
 def game_hash
 {:home => {
-  :team_name => {},
-  :colors => {},
+  :team_name => "Brooklyn Nets",
+  :colors => ["Black", "White"],
   :players => [{
     :player_name => "",
     :number => "",
