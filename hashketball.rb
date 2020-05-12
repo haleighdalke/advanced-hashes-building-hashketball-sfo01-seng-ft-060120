8df04d15cc8fb5915b330e5b879ffa315467bd43
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-12 20:37:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,6 +2,17 @@
 require 'pry'
 
 def game_hash
+{:home => {
+  :team_name => {},
+  :colors => {},
+  :players => [{}]
+},
 
-  {}
+ :away => {
+   :team_name => {},
+   :colors => {},
+   :players => [{}]
+ }
+
+}
 end
